d36005d6ca43e0e180bbff2114a7ccceee3a1319
JeongJun <rgfdds98@gmail.com>
2024-12-08 22:09:24 +0900

    design: create answer detail dialog component
    answer delete dialog added as well

--- src/components/answer/AnswerList.tsx
+++ src/components/answer/AnswerList.tsx
@@ -1,4 +1,5 @@
 import { ScrollArea } from "../ui/scroll-area";
+
 import AnswerCard from "./AnswerCard";
 
 import { Marble } from "@/pages/answer-result/type/marble";
@@ -6,16 +7,23 @@ import { Marble } from "@/pages/answer-result/type/marble";
 // TODO: 임시 선언이므로 업데이트 필요
 interface Props {
   listData: Marble[];
+  onDialogOpen: (marble: Marble) => void;
 }
 
-export default function AnswerList({ listData }: Props) {
+export default function AnswerList({ listData, onDialogOpen }: Props) {
   const answerData: Marble[] = listData;
 
   return (
     <ScrollArea className="h-[580px]">
       <div className="flex flex-col gap-3 px-3">
-        {answerData.map((a, index) => {
-          return <AnswerCard key={index} answer={a} />; // TODO: key값 업데이트 필요
+        {answerData.map((marble, index) => {
+          return (
+            <AnswerCard
+              key={index}
+              answer={marble}
+              onDialogOpen={() => onDialogOpen(marble)}
+            />
+          ); // TODO: key값 업데이트 필요
         })}
       </div>
     </ScrollArea>
--- src/pages/answer-result/index.tsx
+++ src/pages/answer-result/index.tsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
+
 import AnswerList from "@/components/answer/AnswerList";
+import AnswerDetailDialog from "@/components/answer/dialog/AnswerDetailDialog";
+
+import { Marble } from "./type/marble";
+
 import dummyData from "@/components/answer/dummy.json";
 
 export default function AnswerResult() {
-  return (
-    <div>
-      <div className="text-center pt-20 pb-10 mb-3">
-        <h2 className="font-bold text-h2 text-primary">사용자님의 보따리</h2>
-        <h5 className="font-bold text-h5">
-          {dummyData.answerData.length}개의 답변이 담겨 있어요!
-        </h5>
-      </div>
+  const [isDetailDialogOpen, setIsDetailDialogOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<Marble | null>(null);
 
-      <AnswerList listData={dummyData.answerData} />
-    </div>
+  const handleDialogToggle = (marble?: Marble) => {
+    setIsDetailDialogOpen(!isDetailDialogOpen);
+
+    if (marble) {
+      setSelectedItem(marble);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <div className="text-center pt-20 pb-10 mb-3">
+          <h2 className="font-bold text-h2 text-primary">사용자님의 보따리</h2>
+          <h5 className="font-bold text-h5">
+            {dummyData.answerData.length}개의 답변이 담겨 있어요!
+          </h5>
+        </div>
+        <AnswerList
+          listData={dummyData.answerData}
+          onDialogOpen={handleDialogToggle}
+        />
+      </div>
+      {selectedItem && (
+        <AnswerDetailDialog
+          isOpen={isDetailDialogOpen}
+          onClose={handleDialogToggle}
+          data={selectedItem}
+        />
+      )}
+    </>
   );
 }
