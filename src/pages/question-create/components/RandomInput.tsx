f6d1ea1bf78d198fcede1fcb4186bb58d0ba93da
s2oy <w2032@e-mirim.hs.kr>
2024-12-08 14:44:30 +0900

    fix: component interface변경 및 disabled조건 추가

--- src/pages/question-create/components/RandomInput.tsx
+++ src/pages/question-create/components/RandomInput.tsx
@@ -1,26 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { getRandomIndex } from "@/lib/utils";
-import { useState } from "react";
 import { MOCK_QUESTION_LIST } from "../mock";
 
-export default function RandomInput() {
-  const [question, setQuestion] = useState<string>("");
+interface RandomInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export default function RandomInput({ value, onChange }: RandomInputProps) {
   const randomQuestion = MOCK_QUESTION_LIST[getRandomIndex(MOCK_QUESTION_LIST)];
 
-  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuestion(e.target.value);
+  const handleChangeInput = (value: string) => {
+    onChange(value);
   };
 
   return (
     <div className="flex gap-2">
       <Input
-        value={question}
+        value={value}
         className="w-64"
         placeholder="문구를 입력해주세요"
-        onChange={handleChangeInput}
+        onChange={({ target: { value } }) => handleChangeInput(value)}
       />
-      <Button onClick={() => setQuestion(randomQuestion)}>랜덤</Button>
+      <Button onClick={() => handleChangeInput(randomQuestion)}>랜덤</Button>
     </div>
   );
 }
--- src/pages/question-create/index.tsx
+++ src/pages/question-create/index.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
 import RandomInput from "./components/RandomInput";
+import { useState } from "react";
 
 const BUNDEL_IMAGE_URL =
   "https://img.freepik.com/premium-vector/santa-s-sack-pouch-sack-isolated-white-background-vector-illustration_650542-580.jpg";
 
 export default function QuestionCreate() {
+  const [question, setQuestion] = useState<string>("");
+
+  const handleChangeQuestion = (value: string) => {
+    setQuestion(value);
+  };
+
   return (
     <section className="flex flex-col items-center h-screen">
       <div className="flex flex-col items-center gap-4 grow">
@@ -20,11 +27,17 @@ export default function QuestionCreate() {
         <span>
           <b>대답을 담을 보따리에 어떤 문구를 붙일까요?</b>
         </span>
-        <RandomInput />
+        <RandomInput value={question} onChange={handleChangeQuestion} />
       </div>
 
       <div className="p-12">
-        <Button children="다음" />
+        <Button
+          disabled={!question.trim().length}
+          onClick={() => {
+            // TODO: Detail Page로 direct되도록 로직 추가 필요
+          }}
+          children="다음"
+        />
       </div>
     </section>
   );
