581169894ee8e61cb5663cd3651e1adc0e1c22fc
hyjoong <maplesosser@naver.com>
2024-12-15 22:58:59 +0900

    feat: settings sheet 컴포넌트 조건부 렌더링 구현 - /main page인 경우에만 노출

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import GoBackHeader from "@/components/go-back-header/GoBackHeader";
 import SettingsSheet from "@/components/settings-sheet/SettingsSheet";
 
 export default function Header() {
+  const history = useHistory();
+  const [isMainPage, setIsMainPage] = useState(
+    history.location.pathname === "/main"
+  );
+
+  useEffect(() => {
+    const unlisten = history.listen((location) => {
+      setIsMainPage(location.pathname === "/main");
+    });
+
+    return () => {
+      unlisten();
+    };
+  }, [history]);
+
   return (
     <div className="flex items-center justify-between">
       <GoBackHeader />
-      <SettingsSheet />
+      {isMainPage && <SettingsSheet />}
     </div>
   );
 }
