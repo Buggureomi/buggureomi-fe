97a5921dae75c285787a4f558930cfe83014b048
JeongJun <rgfdds98@gmail.com>
2024-12-24 17:16:07 +0900

    refactor: add token api result and error checking routine

--- src/pages/oauth/index.tsx
+++ src/pages/oauth/index.tsx
@@ -16,22 +16,46 @@ export default function OAuth() {
   if (code) {
     memberAPI.getToken({ code }).then((res) => {
       const data = res.data;
-      if (res.data.status === "OK") {
-        if (data) {
-          tokenCookie.setCookie("accessToken", data.data.accessToken, 0.25);
-          tokenCookie.setCookie("refreshToken", data.data.refreshToken, 1);
+
+      try {
+        if (res.data.status === "OK") {
+          if (data.data) {
+            tokenCookie.setCookie("accessToken", data.data.accessToken, 0.25);
+            tokenCookie.setCookie("refreshToken", data.data.refreshToken, 1);
+          }
+
+          if (tokenCookie.getCookie("accessToken")) {
+            memberAPI.search().then((res) => {
+              const data = res.data;
+
+              if (data.status === "OK") {
+                setUserInfo(data.data);
+                history.push("/main");
+              } else {
+                throw new Error("Fail to get user information");
+              }
+            });
+          } else {
+            throw new Error(
+              "Token API call is 'OK', but there is no accessToken"
+            );
+          }
+        } else {
+          throw new Error("Fail to receive user token");
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          if (error.message === "Fail to get user information") {
+            history.push("/main");
+          } else if (
+            error.message === "Fail to receive user token" ||
+            error.message ===
+              "Token API call is 'OK', but there is no accessToken"
+          ) {
+            history.push("/member-login");
+          }
         }
       }
-
-      memberAPI.search().then((res) => {
-        const data = res.data;
-
-        if (data.status === "OK") {
-          setUserInfo(data.data);
-        }
-
-        history.push("/main");
-      });
     });
   }
 
