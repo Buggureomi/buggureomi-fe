b36508146a1583e9ff7c293bd94780569bac1e96
JeongJun <rgfdds98@gmail.com>
2025-01-04 02:07:27 +0900

    refactor: modify header back button feature
    It's going to back path, instead of main path

--- src/components/back-header/BackHeader.tsx
+++ src/components/back-header/BackHeader.tsx
@@ -1,25 +1,20 @@
-import { ReactNode } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { cn } from "@/lib/utils";
 import { useHistory } from "react-router-dom";
 
 type Props = {
-  children?: ReactNode;
   className?: string;
 };
 
-export default function BackHeader({ children, className }: Props) {
+export default function BackHeader({ className }: Props) {
   const history = useHistory();
   const handleBack = () => {
-    history.push("/main");
+    history.goBack();
   };
 
   return (
-    <header className={cn("relative", className)}>
-      <button onClick={handleBack}>
-        <FaArrowLeft size={32} color="#F0F0F0" />
-      </button>
-      {children}
-    </header>
+    <button onClick={handleBack} className={cn("relative", className)}>
+      <FaArrowLeft size={32} color="#F0F0F0" />
+    </button>
   );
 }
