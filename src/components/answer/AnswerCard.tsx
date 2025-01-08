6582531886687b4ca82380f949cb9556ed14128f
JeongJun <rgfdds98@gmail.com>
2025-01-08 17:24:33 +0900

    fix: apply line break effect property

--- src/components/answer/AnswerCard.tsx
+++ src/components/answer/AnswerCard.tsx
@@ -21,7 +21,7 @@ export default function AnswerCard({ answer, onDialogOpen }: Props) {
 
       <div
         onClick={onDialogOpen}
-        className="rounded-md border-2 min-h-20 p-3 bg-[#F3F3F3] font-nanum-dahaengce text-xl"
+        className="rounded-md border-2 min-h-20 p-3 bg-[#F3F3F3] font-nanum-dahaengce text-xl whitespace-pre-wrap"
       >
         <p>{answer.content}</p>
       </div>
